f06221857b8419ecdec55a94093a48dfcfa1ddf7
alex <alex_kobilianskiy@bigmir.net>
2019-10-27 19:44:14 +0200

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
 
+    /**
+     * mobile-mnu customization
+     */
+    var mmenu = $('#mobile-mnu');
+    var $mmenu = mmenu.mmenu({
+        "pageScroll": true,
+
+        "navbar": {
+            "title" : "",
+        },
+        "extensions": [
+            "position-front",
+            "fullscreen",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-container"
+        },
+        "autoHeight": true
+    });
+
+    var mmenuBtn = $("#mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+    $('#close-mnu').click(function(e){
+        e.preventDefault();
+        API.close();
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    /**
+     * end mobile-mnu customization
+     */
+
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
